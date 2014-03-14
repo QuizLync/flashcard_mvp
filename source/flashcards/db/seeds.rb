8af3498a3ea93e720129dd66a841b4ab6f0ba5b6
Drew Parker & Eric Jos Johnson <pair+drewcp87.ericjosjohnson@devbootcamp.com>
2014-03-14 15:32:35 -0700

    Seed state data works

--- source/flashcards/db/seeds.rb
+++ source/flashcards/db/seeds.rb
@@ -1,12 +1,17 @@
 # Get input data
 require 'csv'
+TXT_FILE = 'db/state_capitals_deck.txt'
+# require_relative '/state_capitals'
 
-new_deck = Deck.create(name: "State Capitals")
+@new_deck = Deck.create(name: "State Capitals")
 
-File.open('state_capitals.csv').read.split("\n").each do |line|
+File.open(TXT_FILE).read.split("\n").each do |line|
   state, capital = line.split(", ")
-  new_card = Card.create[question: state, answer: capital]
-  new_deck << new_card
+  # @new_deck <<
+  @new_card = Card.create(question: state, answer: capital)
+  @new_deck.cards << @new_card
+  @new_deck.save
 end
 
-p new_deck
+# p @new_deck.cards << @new_card
+
