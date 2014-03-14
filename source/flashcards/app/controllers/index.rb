4a01fc40537aac5d035f169fc25d93f47be81b31
Christine Dolendo <chris.dolendo@gmail.com>
2014-03-14 15:02:38 -0700

    working controllers, viewers and models. sans seeds

--- source/flashcards/app/controllers/index.rb
+++ source/flashcards/app/controllers/index.rb
@@ -1,47 +1,63 @@
-#notes page featuring a create notes form, and lists all the notes that have been created below
+#homepage, has welcome message, lists all the decks
 get '/' do
+  @decks = Deck.all
   erb :index
 end
 
-#shows note page
-get '/notes/:id' do
-  @note = Note.find(params[:id])
-  @note_page = true
-  erb :index
+#showing the card
+get '/decks/:id' do
+  @deck = Deck.find(params[:id])
+  @card = @deck.select_card
+  erb :card
 end
 
-#creates new note from form on index
-post '/notes' do
-  @note = Note.create(params)
-  redirect "/notes/#{@note.id}"
-end
-
-#shows update/delete form
-get '/notes/:id/edit' do
-  @note = Note.find(params[:id])
-  @edit_note = true
-  erb :index
-end
-
-
-#update database!
-put '/notes/:id' do
+#processing the answer submission
+post '/cards/:id' do 
   p params
-  @note = Note.find(params[:id])
-  @note.update_attributes(title: params[:title], content: params[:content])
-  redirect "/notes/#{@note.id}"
+  @card = Card.find(params[:id])
+  @user_answer = params[:answer]
+  check_answer
 end
 
-#verify deletion
-get '/notes/:id/verify_deletion' do
-  @note = Note.find(params[:id])
-  @verify = true
-  erb :index
-end
+# #shows note page
+# get '/notes/:id' do
+#   @note = Note.find(params[:id])
+#   @note_page = true
+#   erb :index
+# end
 
-#delete the post from database
-delete '/notes/:id' do
-  @note = Note.find(params[:id])
-  @note.destroy
-  redirect "/"
-end
+# #creates new note from form on index
+# post '/notes' do
+#   @note = Note.create(params)
+#   redirect "/notes/#{@note.id}"
+# end
+
+# #shows update/delete form
+# get '/notes/:id/edit' do
+#   @note = Note.find(params[:id])
+#   @edit_note = true
+#   erb :index
+# end
+
+
+# #update database!
+# put '/notes/:id' do
+#   p params
+#   @note = Note.find(params[:id])
+#   @note.update_attributes(title: params[:title], content: params[:content])
+#   redirect "/notes/#{@note.id}"
+# end
+
+# #verify deletion
+# get '/notes/:id/verify_deletion' do
+#   @note = Note.find(params[:id])
+#   @verify = true
+#   erb :index
+# end
+
+# #delete the post from database
+# delete '/notes/:id' do
+#   @note = Note.find(params[:id])
+#   @note.destroy
+#   redirect "/"
+# end
--- source/flashcards/app/models/deck.rb
+++ source/flashcards/app/models/deck.rb
@@ -5,11 +5,7 @@ class Deck < ActiveRecord::Base
     @card = self.cards.sample #takes a random card
   end
 
-  def answer_correct?(card, user_answer) #takes the user's text input
-    @card = card
-    @user_answer = user_answer
-    @card.answer == @user_answer #will return true or false
-  end
 
 end
+
 
