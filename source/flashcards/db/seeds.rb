7aaa0728b5ea3b15ebc43ff0986ca9525febb301
Drew Parker & Eric Jos Johnson <pair+drewcp87.ericjosjohnson@devbootcamp.com>
2014-03-14 15:53:10 -0700

    Ruby Flashcards seeding good

--- source/flashcards/db/seeds.rb
+++ source/flashcards/db/seeds.rb
@@ -1,10 +1,10 @@
 # Get input data
 require 'csv'
 TXT_FILE = 'db/state_capitals_deck.txt'
+RUBYCARDS_FILE = 'db/flashcard_samples.txt'
 # require_relative '/state_capitals'
 
 @new_deck = Deck.create(name: "State Capitals")
-
 File.open(TXT_FILE).read.split("\n").each do |line|
   state, capital = line.split(", ")
   # @new_deck <<
@@ -13,5 +13,16 @@ File.open(TXT_FILE).read.split("\n").each do |line|
   @new_deck.save
 end
 
-# p @new_deck.cards << @new_card
+
+@ruby_deck = Deck.create(name: "Ruby Definitions")
+flashcard_lines = File.readlines(RUBYCARDS_FILE)
+flashcard_lines.delete_if { |line| line == "\n" }
+flashcard_lines.map! { |line| line.chomp }
+while flashcard_lines.length > 1
+  definition,word = flashcard_lines.shift(2)
+  @new_card = Card.create(question: definition, answer: word)
+  @ruby_deck.cards << @new_card
+  @ruby_deck.save
+end
+
 
