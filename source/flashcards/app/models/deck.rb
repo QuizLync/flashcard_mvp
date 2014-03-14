8ad771accc3a7538fa22c1fd291355781301da71
Christine Dolendo <chris.dolendo@gmail.com>
2014-03-14 12:28:05 -0700

    Added get_card and answer_correct methods to Deck model. Tests passed very well.

--- source/flashcards/app/models/deck.rb
+++ source/flashcards/app/models/deck.rb
@@ -1,4 +1,15 @@
 class Deck < ActiveRecord::Base
   has_many :cards
 
+  def select_card #on deck object
+    @card = self.cards.sample #takes a random card
+  end
+
+  def answer_correct?(card, user_answer) #takes the user's text input
+    @card = card
+    @user_answer = user_answer
+    @card.answer == @user_answer #will return true or false
+  end
+
 end
+
