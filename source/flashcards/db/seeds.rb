dd48aa575ead70a43f3eb2525604b273ad1e388b
Drew Parker & Eric Jos Johnson <pair+drewcp87.ericjosjohnson@devbootcamp.com>
2014-03-14 16:06:13 -0700

    third database added. all databases seeded

--- source/flashcards/db/seeds.rb
+++ source/flashcards/db/seeds.rb
@@ -1,18 +1,16 @@
 # Get input data
 require 'csv'
 TXT_FILE = 'db/state_capitals_deck.txt'
+CSV_FILE = 'db/ncaa_basketball_champions.txt'
 RUBYCARDS_FILE = 'db/flashcard_samples.txt'
-# require_relative '/state_capitals'
 
 @new_deck = Deck.create(name: "State Capitals")
 File.open(TXT_FILE).read.split("\n").each do |line|
   state, capital = line.split(", ")
-  # @new_deck <<
   @new_card = Card.create(question: state, answer: capital)
   @new_deck.cards << @new_card
   @new_deck.save
 end
-
 
 @ruby_deck = Deck.create(name: "Ruby Definitions")
 flashcard_lines = File.readlines(RUBYCARDS_FILE)
@@ -25,4 +23,11 @@ while flashcard_lines.length > 1
   @ruby_deck.save
 end
 
-
+@new_deck = Deck.create(name: "NCAA Basketball Champions")
+File.open(CSV_FILE).read.split("\n").each do |line|
+  year, team, player, year2, team2, player2 = line.split(",")
+  @new_year = year.split(" ").first
+  @new_card = Card.create(question: @new_year, answer: team)
+  @new_deck.cards << @new_card
+  @new_deck.save
+end
